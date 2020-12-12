70bb1722ba692c75f6850bf67118a40465fb0809
Srithan <srithanrudrangi@gmail.com>
2020-12-12 09:35:39 -0500

    few

--- program6_1.py
+++ program6_1.py
@@ -1,9 +1,10 @@
 def main():
     outfile = open('philosophers.txt', 'w')
-    outfile.write('John Locke\n')
+    outfile.write('Srithan Rudrangi\n')
     outfile.write('David Hume\n')
     outfile.write('Edmund Burke\n')
     outfile.close()
+    print("Created file with above data.")
 
 
 main()
